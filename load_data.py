15b78ab5971c19a1ef3b71d8a21131af3f570abf
tomasz-stepien <tomaszstepien03@gmail.com>
2018-08-17 21:25:03 +0200

    added loading training, validation and test sets
    Signed-off-by: tomasz-stepien <tomaszstepien03@gmail.com>

--- load_data.py
+++ load_data.py
@@ -3,29 +3,21 @@
 """loads images to np.arrays"""
 
 import os
-import random
-import shutil
-from collections import defaultdict
-from os import listdir
-from os.path import isfile, join
 from random import shuffle
 
 import numpy as np
 from keras.preprocessing.image import img_to_array, load_img
-from keras.utils import to_categorical
-
-import config
-from src.tools import processing
 
 
-def _load_images_into_array(path, pic_size=config.PIC_SIZE, sample_size=-1):
-    """iterates over a directory and reads all images
-    into an ndarray with dimensions (nfiles, width, height, 3)
-    assumes all files in the given directory are images
-
-    :return: ndarray - 4d (nfiles, width, height, 3)
+def load_images_into_array(directory, pic_size=(10, 10), sample_size=-1):
     """
-    files = os.listdir(path)
+
+    :param directory: directory containing pictures
+    :param pic_size:
+    :param sample_size: integer - how many images should be loaded, if -1 then all pictures are loaded
+    :return: np.Array with dims (sample_size, width, height, channels)
+    """
+    files = os.listdir(directory)
     if sample_size > len(files):
         print("sample_size > len(files)")
     elif sample_size > 0:
@@ -35,307 +27,63 @@ def _load_images_into_array(path, pic_size=config.PIC_SIZE, sample_size=-1):
     for file in files:
         # noinspection PyBroadException
         try:
-            img = img_to_array(load_img(f"{path}/{file}", target_size=pic_size)) / 127.5 - 1.
+            img = img_to_array(load_img(f"{directory}/{file}", target_size=pic_size)) / 127.5 - 1.
             temp_list.append(img)
         except Exception:
-            print(f"problem with: {path}/{file}")
-            pass
+            print(f"problem with: {directory}/{file}")
 
     return np.stack(temp_list, axis=0)
 
 
-def _prepare_dataset(x, y, classes_to_read):
-    """divides datasets for x and y
-
-    :param x:
-    :param y:
-    :param classes_to_read: number of classes
-    :return: ready x and y for given type of dataset
-    """
-    x_dataset = np.concatenate(x, axis=0)
-    y_dataset = np.array(y)
-    shuffle_dataset = np.random.permutation(x_dataset.shape[0])
-    x_dataset = np.array(x_dataset[shuffle_dataset, :, :])
-    y_dataset = y_dataset[shuffle_dataset]
-    y_dataset = to_categorical(y_dataset, num_classes=len(classes_to_read))
-    x_dataset = np.array(x_dataset).astype(np.float32)
-
-    return x_dataset, y_dataset
-
-
-def _prepare_classification_folders(folders_list, letters, gan=False):
+def load_train_valid_test_arrays(classes_dict, directory='dataset/'):
     """
 
-    :param folders_list:
-    :param letters:
-    :param gan:
+    :param classes_dict: dictionary with classes and train, valid, test sample number eg.
+    dictionary = {'A': (10, 10, 10),
+                  'D': (10, 10, 10)
+                 }
+
+    :param directory: directory which contains all classes subfolders
+    :return: tuple of 6 numpy arrays
     """
-    for path in folders_list:
-        current_path = os.path.join(path, f"{letters[0]}_{letters[1]}")
-        _prepare_folder(path)
-        _prepare_folder(current_path)
-        _prepare_folder(os.path.join(current_path, 'train'))
-        _prepare_folder(os.path.join(current_path, 'validation'))
-        _prepare_folder(os.path.join(current_path, 'test'))
-        if gan:
-            _prepare_folder(os.path.join(current_path, 'generated'))
-
-        for letter in letters:
-            _remove_trash(os.path.join(current_path, 'train', letter))
-            _remove_trash(os.path.join(current_path, 'validation', letter))
-            _remove_trash(os.path.join(current_path, 'test', letter))
-
-            _prepare_folder(os.path.join(current_path, 'train', letter))
-            _prepare_folder(os.path.join(current_path, 'validation', letter))
-            _prepare_folder(os.path.join(current_path, 'test', letter))
-            if gan:
-                _remove_trash(os.path.join(current_path, 'generated', letter))
-                _prepare_folder(os.path.join(current_path, 'generated', letter))
-
-
-def load_sets(classes_to_read,
-              path=config.DATA_PATH,
-              pic_size=config.PIC_SIZE,
-              sample_size=(-1, -1)):
-    """reads train and valid pictures into keras friendly arrays
-    assumes that each class has a sepearate directory with a proper name
-    eg.
-    # todo: complete documentation
-
-    :param pic_size:
-    :param path:
-    :param classes_to_read: should match names of proper directories
-    :param sample_size: tuple -1 means all images
-    :return: tuple of ndarrays (x_train, y_train, x_valid, y_valid), shuffled
-    """
-
-    if path[-1] != '/':
-        path += '/'
 
     x_train = []
-    x_valid = []
     y_train = []
+
+    x_valid = []
     y_valid = []
-    label = 0
 
-    for c in classes_to_read:
-        for f in os.listdir(f"{path}{c}"):
-            os.rename(f"{path}{c}/{f}", f"{path}{c}/" + f.replace('=', ''))
-        loaded = _load_images_into_array(path=f"{path}{c}", pic_size=pic_size, sample_size=sample_size[0])
-        x_train.append(loaded)
-        y_train += [label] * loaded.shape[0]
+    x_test = []
+    y_test = []
 
-        label += 1
+    i = 0  # class binary label
 
-    x_train, y_train = _prepare_dataset(x=x_train, y=y_train, classes_to_read=classes_to_read)
+    for class_ in classes_dict:
+        samples = load_images_into_array(directory + class_, sample_size=sum(classes_dict[class_]))
 
-    return x_train, y_train, x_valid, y_valid
+        x1, x2, x3 = np.vsplit(samples[np.random.permutation(samples.shape[0])],
+                               (classes_dict[class_][0], classes_dict[class_][0] + classes_dict[class_][1]))
 
+        x_train.append(x1)
+        x_valid.append(x2)
+        x_test.append(x3)
 
-def prepare_final_datasets(letters):
-    """
-    Remove old directories, create new one.
-    Randomly choose pictures from large dataset and copy them to created folders
-    """
+        y1 = np.array([i] * classes_dict[class_][0])
+        y2 = np.array([i] * classes_dict[class_][1])
+        y3 = np.array([i] * classes_dict[class_][2])
 
-    _prepare_folder(config.PATH_FINAL_DATA)
-    _prepare_folder(config.PATH_ROOT)
-    _prepare_folder(config.PATH_STATS)
-    _prepare_folder(config.PATH_STATS_GAN)
-    _prepare_folder(config.PATH_GAN_LETTERS)
-    _prepare_folder(config.PATH_CLASS_LETTERS)
+        y_train.append(y1)
+        y_valid.append(y2)
+        y_test.append(y3)
 
-    _prepare_classification_folders(config.PATH_CLASS_MAX_HALF_TEN, letters)
-    _prepare_classification_folders(config.PATH_CLASS_GAN, letters, gan=True)
+        i += 1
 
-    """ PREPARE SUBFOLDERS WITH LETTERS"""
-    for letter in letters:
-        # GAN FILES
-        for path in config.PATH_GAN_MAX_HALF_TEN:
-            _remove_trash(path + letter)
-            _prepare_folder(path + letter)
+    x_train = np.concatenate(x_train, axis=0)
+    x_valid = np.concatenate(x_valid, axis=0)
+    x_test = np.concatenate(x_test, axis=0)
 
-    letters_dict = defaultdict(dict)
+    y_train = np.concatenate(y_train, axis=0)
+    y_valid = np.concatenate(y_valid, axis=0)
+    y_test = np.concatenate(y_test, axis=0)
 
-    for letter in letters:
-        """ Prepare list of letters which will be moved copied into folders """
-        letters_all = processing.return_all_files(os.path.join(config.DATA_PATH, letter))
-        letters_dict[letter]['max'] = random.sample(letters_all, int(config.DATASET_MAX + config.DATASET_MAX * 0.2))
-        letters_dict[letter]['test'] = random.sample(letters_dict[letter]['max'], int(config.DATASET_MAX * 0.2))
-
-        letters_dict[letter]['max'] = list(set(letters_dict[letter]['max']) - set(letters_dict[letter]['test']))
-        letters_dict[letter]['half'] = random.sample(letters_dict[letter]['max'], int(config.DATASET_MAX / 2))
-        letters_dict[letter]['ten_p'] = random.sample(letters_dict[letter]['half'], int(config.DATASET_MAX * 0.1))
-
-        # COPY FILES TO GAN
-        _copy_files(source_folder=os.path.join(config.DATA_PATH, letter, ''),
-                    destination_folder=os.path.join(config.PATH_GAN_MAX + letter, ''),
-                    files=letters_dict[letter]['max'],
-                    letter=letter)
-        _copy_files(source_folder=os.path.join(config.DATA_PATH, letter, ''),
-                    destination_folder=os.path.join(config.PATH_GAN_HALF + letter, ''),
-                    files=letters_dict[letter]['half'],
-                    letter=letter)
-        _copy_files(source_folder=os.path.join(config.DATA_PATH, letter, ''),
-                    destination_folder=os.path.join(config.PATH_GAN_TEN_P + letter, ''),
-                    files=letters_dict[letter]['ten_p'],
-                    letter=letter)
-
-    """ Split files into train, valid and test folder and copy it into these fold """
-    split_to_train_valid(letters=letters,
-                         letters_dict=letters_dict,
-                         first_letter=0)
-
-    # COPY FILES TO CLASS (+ GENERATED PHOTOS)
-    copy_files_to_class_generated(letters=letters,
-                                  letters_dict=letters_dict,
-                                  first_letter=0)
-
-    split_to_train_valid(letters=letters,
-                         letters_dict=letters_dict,
-                         first_letter=1)
-
-    # COPY FILES TO CLASS (+ GENERATED PHOTOS)
-    copy_files_to_class_generated(letters=letters,
-                                  letters_dict=letters_dict,
-                                  first_letter=1)
-
-
-def copy_files_to_class_generated(letters, letters_dict, first_letter):
-    """
-    Funtion for moving files into generated and test folders in loop depending on option
-    ('max', 'half' and 'ten percent')
-    :param letters: letters on which we are doing operations
-    :param letters_dict: letters dictionary
-    :param first_letter: boolean value - tells if operation is on first or second letter from list
-    :return:
-    """
-    for option in ('max', 'half', 'ten_p'):
-        _copy_files(source_folder=os.path.join(config.DATA_PATH, letters[first_letter], ''),
-                    destination_folder=os.path.join(config.PATH_CLASS_GAN_MAX, f"{letters[0]}_{letters[1]}",
-                                                    'generated', letters[first_letter], ''),
-                    files=letters_dict[letters[first_letter]][option],
-                    letter=letters[1])
-    _copy_files(source_folder=os.path.join(config.DATA_PATH, letters[first_letter], ''),
-                destination_folder=os.path.join(config.PATH_CLASS_GAN_TEN_P, f"{letters[0]}_{letters[1]}",
-                                                'test', letters[first_letter], ''),
-                files=letters_dict[letters[first_letter]]['test'],
-                letter=letters[first_letter])
-
-
-def split_to_train_valid(letters, letters_dict, first_letter):
-    """
-    Funtion for splitting files into train, validation and test folders in loop depending on option
-    ('max', 'half' and 'ten percent')
-    :param letters: letters on which we are doing operations
-    :param letters_dict: letters dictionary
-    :param first_letter: boolean value - tells if operation is on first or second letter from list
-    :return:
-    """
-    for option in ['max', 'half', 'ten_p']:
-        train_validation_test_dividing(source_path=config.PATH_GAN_MAX + letters[first_letter],
-                                       destination_path=os.path.join(config.PATH_CLASS_MAX,
-                                                                     f"{letters[0]}_{letters[1]}"),
-                                       files=letters_dict[letters[first_letter]][option],
-                                       test_files=letters_dict[letters[first_letter]]['test'],
-                                       letter=letters[first_letter],
-                                       percentage=config.PROPORTION)
-
-
-def train_validation_test_dividing(source_path, destination_path, files, test_files, letter, percentage):
-    """
-    Function for dividing list of letters into train and validation set.
-    It also gets test_files list and moves it into test folder
-    :param source_path: paths from which images should be copied
-    :param destination_path: paths for which images should be copied
-    :param files: list of images which should be splitted into training and validation set
-    :param test_files: list of files which should be copied into test folder
-    :param letter: letter on which we are doing operations
-    :param percentage: on what proportion dataset should be splitted into training and validation set
-    :return: fill train, validation and test folders
-    """
-    if len(files) == 0:
-        files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
-    sample = random.sample(files, int(len(files) * percentage))
-    train = [x for x in files if x in sample]
-    valid = [x for x in files if x not in sample]
-
-    _copy_files(source_folder=os.path.join(source_path, ''),
-                destination_folder=os.path.join(destination_path, 'train', letter, ''),
-                files=train,
-                letter=letter)
-    _copy_files(source_folder=os.path.join(source_path, ''),
-                destination_folder=os.path.join(destination_path, 'validation', letter, ''),
-                files=valid,
-                letter=letter)
-    _copy_files(source_folder=os.path.join(config.DATA_PATH, letter, ''),
-                destination_folder=os.path.join(destination_path, 'test', letter, ''),
-                files=test_files,
-                letter=letter)
-
-
-def train_validation_dividing(source_path, destination_path, files, letter, percentage):
-    """
-    Function for dividing list of letters into train and validation set.
-    :param source_path: paths from which images should be copied
-    :param destination_path: paths for which images should be copied
-    :param files: list of images which should be splitted into training and validation set
-    :param letter: letter on which we are doing operations
-    :param percentage: on what proportion dataset should be splitted into training and validation set
-    :return: fill train, validation and test folders
-    """
-    if len(files) == 0:
-        files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
-    sample = random.sample(files, int(len(files) * percentage))
-    train = [x for x in files if x in sample]
-    valid = [x for x in files if x not in sample]
-
-    _copy_files(source_folder=os.path.join(source_path, ''),
-                destination_folder=os.path.join(destination_path, 'train', letter, ''),
-                files=train,
-                letter=letter)
-    _copy_files(source_folder=os.path.join(source_path, ''),
-                destination_folder=os.path.join(destination_path, 'validation', letter, ''),
-                files=valid,
-                letter=letter)
-
-
-def _copy_files(source_folder, destination_folder, files, letter):
-    """
-    Copies files from one folder into another
-    :param source_folder: source folder from which files should be copied
-    :param destination_folder: folder for which files should be copied
-    :param files: list of files on which we are doing operations
-    :param letter: letter on which we are doing operations
-    :return:
-    """
-    for image in files:
-        shutil.copyfile(source_folder + image, destination_folder + image)
-    print(f"{str(len(files))} letters for {letter} copied")
-
-
-def _remove_trash(path):
-    """
-    Remove directory with files
-    :param path: path to the directory
-    """
-
-    # noinspection PyBroadException
-    try:
-        shutil.rmtree(path)
-        print(f"{path} folder deleted")
-    except Exception:
-        print(f"Nothing to delete ({path})")
-
-
-def _prepare_folder(path):
-    """
-    Make directory
-    :param path: path to the directory
-    """
-
-    # noinspection PyBroadException
-    try:
-        os.mkdir(path)
-    except Exception:
-        print(f"{path} directory already exists")
+    return x_train, y_train, x_valid, y_valid, x_test, y_test
--- main.py
+++ main.py
@@ -2,131 +2,21 @@
 
 """magisterka"""
 
-import os
+from random import seed
 
-import tensorflow as tf
+from load_data import load_train_valid_test_arrays
 
-import classifier
-import config
-import gans.dcgan as dc_gan
-import load_data as dl
-from src.tools import processing
+seed(2137)
 
+dictionary = {'A': (10, 10, 10),
+              'D': (10, 10, 10)
+              }
 
-def train_gans(option):
-    """
+x_train, y_train, x_valid, y_valid, x_test, y_test = load_train_valid_test_arrays(dictionary)
 
-    :param option:
-    :return:
-    """
-    # Convert images to numpy arrays
-    x_train, _, _, _ = dl.load_sets(path=config.PATH_GAN_LETTERS,
-                                    sample_size=(int(option[1]), 100),
-                                    classes_to_read=[f"{option[1]}_{letters[1]}"])
-    # CUDA settings
-    c = tf.ConfigProto()
-    c.gpu_options.allow_growth = True
-    # session = tf.Session(config=c)
-
-    # Train GAN
-    dcgan = dc_gan.DCGAN(pic_size=config.PIC_SIZE,
-                         channels=config.CHANNELS)
-    dcgan.train(x_train=x_train,
-                epochs=config.EPOCHS_GAN,
-                batch_size=config.BATCH_SIZE,
-                save_interval=config.SAMPLE_INTERVAL,
-                stats_path=os.path.join(config.PATH_STATS_GAN, f"{option[1]}_{letters[1]}.csv"))
-    return dcgan
-
-
-def generate_images(option, dcgan):
-    """
-
-    :param option:
-    :param dcgan:
-    """
-    path_first_letter = os.path.join(config.PATH_CLASS_LETTERS, f"{option[0]}_{option[1]}_GAN",
-                                     f"{letters[0]}_{letters[1]}",
-                                     'generated', letters[0])
-    path_second_letter = os.path.join(config.PATH_CLASS_LETTERS, f"{option[0]}_{option[1]}_GAN",
-                                      f"{letters[0]}_{letters[1]}",
-                                      'generated', letters[1])
-
-    dcgan.save_imgs(path=path_second_letter, full=True, amount=option[1])
-
-    dl.train_validation_dividing(source_path=path_first_letter,
-                                 destination_path=os.path.join(config.PATH_CLASS_LETTERS,
-                                                               f"{option[0]}_{option[1]}_GAN",
-                                                               f"{letters[0]}_{letters[1]}"),
-                                 files=processing.return_all_files(path_first_letter),
-                                 letter=letters[0],
-                                 percentage=config.PROPORTION)
-    dl.train_validation_dividing(source_path=path_second_letter,
-                                 destination_path=os.path.join(config.PATH_CLASS_LETTERS,
-                                                               f"{option[0]}_{option[1]}_GAN",
-                                                               f"{letters[0]}_{letters[1]}"),
-                                 files=processing.return_all_files(path_second_letter),
-                                 letter=letters[1],
-                                 percentage=config.PROPORTION)
-
-
-def classify_images(path, option, folder):
-    """
-
-    :param path:
-    :param option:
-    :param folder:
-    """
-    img_width, img_height = config.PIC_SIZE[0], config.PIC_SIZE[1]
-    home_path = os.path.join(path, option, folder)
-    print(f"Classification {option} {folder}")
-    if len(os.listdir(os.path.join(home_path, 'train', folder.split('_')[0]))) == 0:
-        print("FOLDER EMPTY!!")
-    else:
-        samples = option.split('_')
-        if len(samples) == 3:
-            amount = int(samples[0]) + int(samples[1]) * 2
-        else:
-            amount = int(samples[0]) + int(samples[1])
-
-        nb_train_samples = amount * config.PROPORTION
-        nb_validation_samples = amount - nb_train_samples
-
-        epochs = config.EPOCHS_CLASS
-        batch_size = config.BATCH_SIZE
-        classifier.train_classifier(home_path, option, folder, img_width, img_height, nb_train_samples,
-                                    nb_validation_samples, epochs, batch_size)
-
-
-if __name__ == "__main__":
-
-    if config.FLAG_PREPARE_DATASETS:
-        processing_time = processing.start_process('datasets preparation')
-        for letters in config.LETTERS:
-            dl.prepare_final_datasets(letters)
-        processing.end_process(processing_time, 'datasets preparation')
-
-    for index, option in enumerate(config.DATASETS_OPTIONS):
-        option = [int(option[0]), int(option[1])]
-        for letters in config.LETTERS:
-            if config.FLAG_TRAIN_GAN:
-                if index == 0:
-                    pass
-                else:
-                    processing_time = processing.start_process('training GAN')
-                    dcgan = train_gans(option)
-                    processing.end_process(processing_time, 'training GAN')
-
-                    if config.FLAG_GENERATE_IMAGES:
-                        processing_time = processing.start_process('generating images')
-                        generate_images(option, dcgan)
-                        processing.end_process(processing_time, 'generating images')
-
-    if config.FLAG_CLASSIFY:
-        processing_time = processing.start_process('classifying images')
-        option_folders = os.listdir(config.PATH_CLASS_LETTERS)
-        for option in option_folders:
-            to_classify = os.listdir(os.path.join(config.PATH_CLASS_LETTERS, option))
-            for folder in to_classify:
-                classify_images(config.PATH_CLASS_LETTERS, option, folder)
-            processing.end_process(processing_time, 'classifying images')
+print(x_train.shape)
+print(y_train.shape)
+print(x_valid.shape)
+print(y_valid.shape)
+print(x_test.shape)
+print(y_test.shape)
